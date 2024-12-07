aa76e218b3825fbb3ab620137b0c8641a9c020cb
tekamyashwant <yashwant.tekam22@gmail.com>
2024-12-07 10:05:35 +0530

    values added

--- Salesforce Journey/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ Salesforce Journey/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,4 @@
 import { LightningElement } from 'lwc';
-
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    valuefromJs='Value from Js file.';
+}
